33b84f5d8a16457ec003d924c203c2b73e74ac92
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-16 09:20:35 +0100

    Rename crCount.php to crCountMain.php
    dummy php

---
+++ crCountMain.php
@@ -0,0 +1,48 @@
+<svg viewBox="0 0 4400 2800"  
+     style="background: #002 ; font-family: 'Catamaran', sans-serif;"  
+     id="zcanvas" version="1.1"  
+     xmlns:xlink="http://www.w3.org/1999/xlink" 
+     xmlns="http://www.w3.org/2000/svg">
+  <title>cash register :: by TRITICUM</title>
+  <defs>
+    <link xmlns="http://www.w3.org/1999/xhtml" href="http://fonts.googleapis.com/css?family=Play" type="text/css" rel="stylesheet" /> 
+    <radialGradient id="date"  cx="0.50" cy="0.50" r="0.50" fx="0.50" fy="0.50"><stop offset="0.00"   stop-color="#ff80ff" /><stop offset="1.00"   stop-color="#804080" /></radialGradient>    
+    <radialGradient id="final" cx="0.50" cy="0.50" r="0.50" fx="0.50" fy="0.50"><stop offset="0.00"   stop-color="#ffff00" /><stop offset="1.00"   stop-color="#ff6000" /></radialGradient>    
+    <radialGradient id="blue"  cx="0.50" cy="0.50" r="0.50" fx="0.50" fy="0.50"><stop offset="0.00"   stop-color="#0000ff" /><stop offset="1.00"   stop-color="#000080" /></radialGradient>    
+    <radialGradient id="push"  cx="0.50" cy="0.50" r="0.50" fx="0.50" fy="0.50"><stop offset="0.00"   stop-color="#ff0000" /><stop offset="1.00"   stop-color="#ff6000" /></radialGradient>    
+    <radialGradient id="red"   cx="0.50" cy="0.50" r="0.50" fx="0.50" fy="0.50"><stop offset="0.00"   stop-color="#ff0000" /><stop offset="1.00"   stop-color="#800000" /></radialGradient>    
+    <symbol id="icon-spinner9" viewBox="0 0 16 16">
+    <title>spinner9</title>
+      <path class="path1" d="M8 0c-4.355 0-7.898 3.481-7.998 7.812 0.092-3.779 2.966-6.812 6.498-6.812 3.59 0 6.5 3.134 6.5 7 0 0.828 0.672 1.5 1.5 1.5s1.5-0.672 1.5-1.5c0-4.418-3.582-8-8-8zM8 16c4.355 0 7.898-3.481 7.998-7.812-0.092 3.779-2.966 6.812-6.498 6.812-3.59 0-6.5-3.134-6.5-7 0-0.828-0.672-1.5-1.5-1.5s-1.5 0.672-1.5 1.5c0 4.418 3.582 8 8 8z"></path>
+    </symbol>
+    <symbol id="icon-loop2" viewBox="0 0 32 32">
+      <title>loop2</title>
+      <path class="path1" d="M27.802 5.197c-2.925-3.194-7.13-5.197-11.803-5.197-8.837 0-16 7.163-16 16h3c0-7.18 5.82-13 13-13 3.844 0 7.298 1.669 9.678 4.322l-4.678 4.678h11v-11l-4.198 4.197z"></path>
+      <path class="path2" d="M29 16c0 7.18-5.82 13-13 13-3.844 0-7.298-1.669-9.678-4.322l4.678-4.678h-11v11l4.197-4.197c2.925 3.194 7.13 5.197 11.803 5.197 8.837 0 16-7.163 16-16h-3z"></path>
+    </symbol>
+    <symbol id="icon-calendar" viewBox="0 0 32 32">
+    <title>calendar</title>
+      <path class="path1" d="M10 12h4v4h-4zM16 12h4v4h-4zM22 12h4v4h-4zM4 24h4v4h-4zM10 24h4v4h-4zM16 24h4v4h-4zM10 18h4v4h-4zM16 18h4v4h-4zM22 18h4v4h-4zM4 18h4v4h-4zM26 0v2h-4v-2h-14v2h-4v-2h-4v32h30v-32h-4zM28 30h-26v-22h26v22z"></path>
+    </symbol>
+    <pattern id="hatch03" patternUnits="userSpaceOnUse" x="0" y="0" width="25" height="25"  viewBox="0 0 25 25" >
+		  <path d="M0,0 L25,25 M25,0 L0,25 "  fill="none" stroke="#003" stroke-width="5" />     
+		</pattern>	
+  </defs>
+  
+  <!--rect x="1" y="1" height="2800" width="4400" style="fill:url(#hatch03)" /-->
+  
+  <g id="buttons">
+    <g font-size="48" fill="#ff6000"><path d="M1550,200 L2800,200 Q2850,200 2850,250 L2850,1800 Q2850,1850 2800,1850 L2400,1850 L2300,1750 L1650,1750 Q1550,1750 1550,1700 L1550,250 Q1550,200 1600,200 Z" fill="#00a" /></g>   
+    <g font-size="48" fill="#666" letter-spacing="12"><text x="4300" y="200" transform="rotate(90, 4300,200)" text-anchor="start">SALES  STATISTIC  LEGAL  UNIT  33609353</text></g>        
+    <g font-size="64" fill="#fff" text-anchor="middle" font-weight="100">    
+      <text    x="1550" y="1840" text-anchor="start" fill="#888" id="zstatus">quo</text>
+      <circle cx="1750" cy="2125" r="125" stroke-width="0" stroke="#0f0" fill="#0a0" zfactor="1" /><text x="1745" y="2140"  id="addsub" >add</text>                
+      <circle cx="2200" cy="2125" r="125" stroke-width="3" stroke="#f00" fill="#f00" zdateselect="true"  />
+        <use xlink:href="#icon-calendar" fill="#fff" x="2154" y="2080" height="96" width="96" zdateselect="true" />
+      <circle cx="2650" cy="2125" r="125" stroke-width="0" stroke="#080" fill="#080" zfinalise="true"   />    
+        <use xlink:href="#icon-loop2" fill="#fff" x="2604" y="2076" height="96" width="96" />
+      <text    x="1550" y="2740" text-anchor="start" fill="#888" id="zmessage">mike was here and try to communicate</text>  
+    </g>    
+  </g>
+  <script type="application/ecmascript" xlink:href="crCount1.js" ></script>    
+</svg>
