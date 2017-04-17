50c30bf826da00ec3e7f6d5b144296ed6846d0c3
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-17 10:37:56 +0100

    Update crCount.php

--- crCount.php
+++ crCount.php
@@ -2,11 +2,13 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1);  
   
-  $fname = dirname(__FILE__) . '\\staticdata\\crCountLog.xml';  
+  $postdata = file_get_contents("php://input"); 
+  
+  $fname = dirname(__FILE__) . '//staticdata//crCountLog.xml';  
   $dom = new DOMDocument; 
   $dom->load($fname, LIBXML_DTDLOAD|LIBXML_DTDATTR); // made by triticum 
   $root = $dom->documentElement;                     // we have the root entry */
-  $postdata = file_get_contents("php://input"); 
+  
   $row = $dom->createDocumentFragment();             // load the client fragment ...
   $row->appendXML($postdata);
   $root->appendChild($row);
