e6df095ccb021c87b2d4b897232320060d076acd
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-18 09:51:08 +0100

    Update crCount.php

--- crCount.php
+++ crCount.php
@@ -2,6 +2,11 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1);  
   use Google\Cloud\Storage\StorageClient;
+
+  // $storage = new StorageClient();
+  // $bucket = $storage->bucket("crcountlog");
+  // $object = $bucket->object('my-object');
+  // $stringXML = $object->downloadAsString();
   
   $postdata = file_get_contents("php://input"); 
   echo $postdata;
@@ -9,6 +14,7 @@
   $fname = dirname(__FILE__) . '//staticdata//crCountLog.xml';  
   $dom = new DOMDocument; 
   $dom->load($fname, LIBXML_DTDLOAD|LIBXML_DTDATTR); // made by triticum 
+  // $dom->loadXML($stringXML); 
   $root = $dom->documentElement;                     // we have the root entry */
   
   $row = $dom->createDocumentFragment();             // load the client fragment ...
