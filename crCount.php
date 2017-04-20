0683aaf227bcea4bba5894413ae9e8577cdb01b2
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-20 07:17:58 +0100

    Update crCount.php

--- crCount.php
+++ crCount.php
@@ -3,24 +3,24 @@
   ini_set("display_errors", 1);  
   use Google\Cloud\Storage\StorageClient;
 
-  // $storage = new StorageClient();
-  // $bucket = $storage->bucket("crcountlog");
-  // $object = $bucket->object('my-object');
-  // $stringXML = $object->downloadAsString();
+  $storage = new StorageClient();
+  $bucket = $storage->bucket("crcountlog");
+  $object = $bucket->object('crCountLog.xml');
+  $stringXML = $object->downloadAsString();
   
   $postdata = file_get_contents("php://input"); 
   echo $postdata;
   
-  $fname = dirname(__FILE__) . '//staticdata//crCountLog.xml';  
+  //$fname = dirname(__FILE__) . '//staticdata//crCountLog.xml';  
   $dom = new DOMDocument; 
-  $dom->load($fname, LIBXML_DTDLOAD|LIBXML_DTDATTR); // made by triticum 
-  // $dom->loadXML($stringXML); 
+  //$dom->load($fname, LIBXML_DTDLOAD|LIBXML_DTDATTR); // made by triticum 
+  $dom->loadXML($stringXML); 
   $root = $dom->documentElement;                     // we have the root entry */
   
   $row = $dom->createDocumentFragment();             // load the client fragment ...
   $row->appendXML($postdata);
   $root->appendChild($row);
-  $dom->save($fname);                                // At this point we save it - casted !
-  // file_put_contents("gs://crcountlog/crCountLog.xml", $dom->saveXML());
+  // $dom->save($fname);                                // At this point we save it - casted !
+  file_put_contents("gs://crcountlog/crCountLog.xml", $dom->saveXML());
   echo "good";
 ?>
