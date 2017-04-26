2ffdb82a28f33fe7635e2bd4369ab9b0225fe29b
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-26 12:25:56 +0200

    Update crCountMain.php

--- crCountMain.php
+++ crCountMain.php
@@ -44,5 +44,5 @@
       <text    x="1550" y="2740" text-anchor="start" fill="#888" id="zmessage">mike was here and try to communicate</text>  
     </g>    
   </g>
-  <script type="application/ecmascript" xlink:href="staticdata/crcount1.js" ></script>    
+  <script type="application/ecmascript" xlink:href="staticdata/crcount.js" ></script>    
 </svg>
