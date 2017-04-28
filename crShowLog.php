948940ff0dc138d721eb393ebc9efb4fb4dbe2fc
Michael Kenneth Rasch <miketriticum@gmail.com>
2017-04-28 08:00:27 +0100

    Update crShowLog.php

--- crShowLog.php
+++ crShowLog.php
@@ -2,19 +2,16 @@
   //header("Content-Type: application/xhtml+xml; charset=utf-8");
   error_reporting(E_ALL);
   ini_set("display_errors", 1);
-  $fname = dirname(__FILE__) . '\\..\\xml\\ac33609353.xml';
-  $xname = dirname(__FILE__) . '\\acGroupLegal.xsl';
-  
   $dom = new DOMDocument;
-  $dom->load($fname, LIBXML_DTDLOAD|LIBXML_DTDATTR);
+  $zFileContents = file_get_contents("gs://crcountlog/crCountLog.xml");
+  $dom->loadXML($zFileContents); 
+
   $xsl = new DOMDocument;
-  $xsl->load($xname);
+  $zFileContents = file_get_contents("gs://crcountlog/crStatic.xsl");
+  $xsl->loadXML($zFileContents); 
   $proc = new XSLTProcessor;
   $proc->importStyleSheet($xsl);
-
-  
   $clientXML = $proc->transformToDoc($dom);
       
   echo $clientXML->saveXML();
-  
 ?>
