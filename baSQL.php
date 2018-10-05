d68858ce60888a4052dff6072648f2393a3b5f35
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-10-05 10:52:40 +0200

    Update baSQL.php

--- baSQL.php
+++ baSQL.php
@@ -1,37 +1,14 @@
 <?php
 
-namespace Google\Cloud\Samples\Bookshelf\DataModel;
 
-use PDO;
-/**
- * Class Sql implements the DataModelInterface with a mysql or postgres database.
- *
- */
-class Sql implements DataModelInterface {
-    private $dsn;
-    private $user;
-    private $password;
+$dsn = getenv('CLOUDSQL_DSN');
+$user = getenv('CLOUDSQL_USER');
+$password = getenv('CLOUDSQL_PASSWORD');
 
-    public function __construct($dsn, $user, $password) {
-        $this->dsn = $dsn;
-        $this->user = $user;
-        $this->password = $password;
+// create the PDO client
+$pdo = new PDO($dsn, $user, $password);
 
-        $pdo = $this->newConnection();
-    }
-    
-    private function newConnection()    {
-        $pdo = new PDO($this->dsn,
-                       $this->user,
-                       $this->password);
-                       
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $pdo;
-    }
-
-    public function listTrxs($search = "%")   {
-    
+     
         $pdo = $this->newConnection();
         $statement = $pdo->prepare("SELECT * FROM trxs WHERE text like ? ORDER BY date desc LIMIT 9999");      
         $statement->bindValue(1, $search, PDO::PARAM_INT);
@@ -43,36 +20,4 @@ class Sql implements DataModelInterface {
         }
 
         return  $rows;
-    }
-
-    public function read($id)    {
-        $pdo = $this->newConnection();
-        $statement = $pdo->prepare('SELECT * FROM trxs WHERE id = ?');
-        $statement->bindValue(1, $id, PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public function update($trx)    {
-        $pdo = $this->newConnection();
-        $statement = $pdo->prepare("UPDATE trxs SET date = ?, text = ?, amount = ? WHERE id = ?");
-        $statement->bindValue(1, $trx->date, PDO::PARAM_STR);
-        $statement->bindValue(2, $trx->text, PDO::PARAM_STR);        
-        $statement->bindValue(3, $trx->amount, PDO::PARAM_INT);        
-        $statement->bindValue(4, $trx->id, PDO::PARAM_INT);                
-        
-        return $statement->execute();
-    }
-
-    public function delete($id)    {
-        $pdo = $this->newConnection();
-        $statement = $pdo->prepare('DELETE FROM trxs WHERE id = ?');
-        $statement->bindValue(1, $id, PDO::PARAM_INT);
-        $statement->execute();
-        // row count is the only info - not raw data ....
-        return $statement->rowCount();
-    }
-
-
-   }
+  ?>
