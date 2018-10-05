dfe5443bedf58edb680cfbfda450d12f25cf572e
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-10-05 10:55:34 +0200

    Update baSQL.php

--- baSQL.php
+++ baSQL.php
@@ -1,15 +1,12 @@
 <?php
 
 
-$dsn = getenv('CLOUDSQL_DSN');
-$user = getenv('CLOUDSQL_USER');
-$password = getenv('CLOUDSQL_PASSWORD');
 
 // create the PDO client
-$pdo = new PDO($dsn, $user, $password);
+$search = "%";
 
-     
-        $pdo = $this->newConnection();
+$pdo = new PDO($connection, $user, $password);
+
         $statement = $pdo->prepare("SELECT * FROM trxs WHERE text like ? ORDER BY date desc LIMIT 9999");      
         $statement->bindValue(1, $search, PDO::PARAM_INT);
         $statement->execute();
