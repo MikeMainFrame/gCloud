0cacadd7fe92c4e0a39c5b62df9f771b95589a35
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-10-14 10:53:56 +0200

    Update baApp.php

--- ba/baApp.php
+++ ba/baApp.php
@@ -2,20 +2,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name=viewport content="width=device-width, initial-scale=0.8">
-    <title>Google Cloud Project Using MySQL and PHP and ECMA 6:: by Mike/TRITICUM :: Q4 2018</title>
+    <title>Google Cloud Project Using MySQL and PHP:: by Mike/TRITICUM :: Q4 2018</title>
+    <style> td { border-bottom: 1px solid #ddd} </style>
   </head>
   <body>
+  <span id="zSearch"  contenteditable="true">Search Criteria %</span>  
   <button id="zButton">_Search_</button>
-  <p id="zSearch"  contenteditable="true">Search Criteria %</p>
-  <pre id="zOutput">Initial state before SQL actions  </pre>
+  <p id="zOutput">Initial state before SQL actions  
 <?php
 
-  echo date(DATE_RFC2822) . " starting baApp.php ....<br />";
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1); 
+
+  echo date(DATE_RFC2822) . " starting baApp.php ....<br />First search will erase these lines ;o)";
   
-  include 'baSQL.php';
+  include 'baSQL.php'; 
 
 ?>
+</p>
 </body>
 <script src="staticdata/ba.js" defer></script>
 </html>
