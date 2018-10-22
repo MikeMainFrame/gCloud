3f1ced58880266ddbef19d04c0ea47dc213a2278
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-10-22 17:54:17 +0200

    Update baSQLload.php

--- ba/baSQLload.php
+++ ba/baSQLload.php
@@ -1,37 +1,21 @@
 <?php
-
 /*
- * M.Rasch 2018
+ * M.Rasch 2018 Q4
 */
-  error_reporting(E_ALL);
-  ini_set("display_errors", 1); 
   
-  $search = "%";
-  
-  if ($_GET) $search = $_GET["search"] . "%";
-  
-  $connection = "mysql:unix_socket=/cloudsql/gcloud19631205:europe-west1:miketriticum2";
-  $user = "root";
-  $password = "MCMLX1i1";
-  
-  $pdo = new PDO($connection, $user, $password); 
-  
-  if ($search === 'load%') {
-	  $temp = file_get_contents("gs://gcloud19631205.appspot.com/insert.sql");
-	  $allData = explode(";\r\n", $temp);
-	  //print_r($allData);
-	  $pdo->beginTransaction();
-	  
-	  for ($ix = 1; $ix < count($allData); $ix++ ) {
-	  	echo $allData[$ix];
-	    $statement = $pdo->prepare($allData[$ix]);      
-	    $statement->execute();		
-	  }
-	  $pdo->commit();
-  }
-  
-  $statement = $pdo->prepare("SELECT * FROM trx.trxs WHERE text LIKE ? ORDER BY date desc LIMIT 9999");      
-  $statement->bindValue(1, $search, PDO::PARAM_STR);
+  $temp = file_get_contents("gs://gcloud19631205.appspot.com/insert.sql");
+  $allData = explode(";\r\n", $temp);
+
+  $pdo->beginTransaction();
+
+    for ($ix = 1; $ix < count($allData); $ix++ ) {
+      $statement = $pdo->prepare($allData[$ix]);      
+      $statement->execute();		
+    }
+
+  $pdo->commit();
+   
+  $statement = $pdo->prepare("SELECT id, date, text, amount FROM trx.trxs ORDER BY date desc LIMIT 9999");     
   $statement->execute();
   
   $collection = array();
