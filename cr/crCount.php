38f9c9ed15e4df42f91d980e2b88fccb5f597199
Michael Kenneth Rasch <miketriticum@gmail.com>
2018-11-02 10:35:04 +0100

    Update crCount.php

--- cr/crCount.php
+++ cr/crCount.php
@@ -1,14 +1,12 @@
 <?php
   error_reporting(E_ALL);
   ini_set("display_errors", 1); 
-  $postdata = file_get_contents("php://input"); 
-  $zFileContents = file_get_contents("gs://crcountlog/crCountLog.xml");
-  $dom = new DOMDocument; 
-  $dom->loadXML($zFileContents); 
-  $root = $dom->documentElement;                     // we have the root entry */
-  $row = $dom->createDocumentFragment();             // load the client fragment ...
-  $row->appendXML($postdata);
+  $DOM = new DOMDocument; 
+  $DOM->loadXML(file_get_contents("gs://crcountlog/crCountLog.xml")); 
+  $root = $DOM->documentElement;                     // we have the root entry */
+  $row = $DOM->createDocumentFragment();             // load the client fragment ...
+  $row->appendXML(file_get_contents("php://input"));
   $root->appendChild($row);
-  file_put_contents("gs://crcountlog/crCountLog.xml", $dom->saveXML());
+  file_put_contents("gs://crcountlog/crCountLog.xml", $DOM->saveXML());
   echo "good";
 ?>
