c5cce02128c555ee5773dc6aa5d5cc876fd602be
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-02-21 18:05:00 +0100

    Update mcTransaction.php

--- mc/mcTransaction.php
+++ mc/mcTransaction.php
@@ -1,5 +1,5 @@
 <?php
-  error_reporting(E_ALL);
+  
   $source = "gs://gcloud19631205.appspot.com/mcMail.xml";
   $mother = new DOMDocument;
   $mother->loadXML(file_get_contents($source));
@@ -7,7 +7,3 @@
   $newNode->appendXML(file_get_contents("php://input"));
   $mother->documentElement->appendChild($newNode);
   file_put_contents($source, $mother->saveXML());
-
-  include 'mcSpontane.php';
-
-?>
